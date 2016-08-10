f09826c332994da38cfb310b0a4c55ead6749586
Matheus Teixeira <matheus.mtxr@gmail.com>
2016-08-09 23:29:36 -0300

    Ter Ago  9 23:29:36 BRT 2016

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -1,10 +1,7 @@
 @if (session()->has('flash_notification.messages'))
     @foreach(session('flash_notification.messages') as $messageData)
-    <div class="alert alert-{{ $messageData['level'] }} {{ $messageData['important'] ? 'alert-important' : '' }}">
-    @if(!$messageData['important'])
+    <div class="flash-message alert alert-{{ $messageData['level'] }} {{ $messageData['important'] ? 'alert-important' : '' }}">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    @endif
-
         {!! trans($messageData['message']) !!}
     </div>
     @endforeach
